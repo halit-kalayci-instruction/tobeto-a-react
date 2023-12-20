11b20027d4a2d138a6f1e310be0b885c85ca1b2b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-20 11:30:14 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './Navbar';
 
@@ -7,17 +7,32 @@ import Navbar from './Navbar';
 function App() {
   // değiştiğinde UI'i da update edecek değişken
   //useState => react fonksiyon (react hooks)
+  //useEffect (fonksiyon, [dep_list])
   let totalCount = 0;
 
   // const [getter, setter] = useState(initial)
   const [total, setTotal] = useState(0);
 
   const increase = () => {
-    setTotal(total + 1);
+    setTotal((prevState) => { return prevState + 1; }); // async
   }
+
   const decrease = () => {
-    setTotal(total - 1);
+    setTotal(total - 1); // async
   }
+  // her sayfa açılışı useEffect'i etkiler
+  // dep listdeki değişkenlerden herhangi biri değiştiğinde tetiklenir.
+  useEffect(() => {
+    console.log("use effect çalıştı")
+  }, []);
+
+  useEffect(() => {
+    console.log("use effect (dependencyli) çalıştı")
+    console.log(total);
+  }, [total]);
+
+  // useState
+  // useEffect 
 
   return (
     <>
@@ -33,3 +48,5 @@ function App() {
 // export const variable2 = 2;
 // const variable3 = 3;
 export default App;
+
+// 11:10
