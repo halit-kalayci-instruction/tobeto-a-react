fd7ffba43064c3a72475e362c7b843a57d9339db
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-20 10:01:44 +0300

    initialize

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import Default, {DefaultOlmayan(lar)} from "dosyaismi";
+import App, { variable, variable2 } from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// self closing tag => <img />
+// normal tag => <a> </a>
+root.render(<App></App>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// 10:15
