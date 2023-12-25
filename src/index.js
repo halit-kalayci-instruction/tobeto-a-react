796c82ed9f669cfff38ffad5036f36352ba9d0b5
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 10:16:10 +0300

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import Default, {DefaultOlmayan(lar)} from "dosyaismi";
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function Homepage() {
 	return (
 		<div>
-			Homepage
+			<button className="btn btn-primary">Homepage</button>
 			<br />
 			{/* SPA'larda href kullanımı yanlıştır. */}
 			<Link to={"/about"}>About'a Git</Link>
