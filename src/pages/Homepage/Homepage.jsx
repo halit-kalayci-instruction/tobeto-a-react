5b30b524a451d96e4c1dd7fc878b087f955f427f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 10:48:40 +0300

    fetch api

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -9,19 +9,42 @@ export default function Homepage() {
 	};
 
 	useEffect(() => {
-		myAsyncFunction()
-			.then(response => {
-				console.log("İşlem başarılı cevap:", response);
-			}) // async işlem başarılı ise (resolve edildi ise)
-			.catch(error => {
-				console.log("İşlem başarısız cevap:", error);
-			}) // async işlem başarısız ise (reject edildi ise)
-			.finally(() => {
-				console.log("İşlem bitti");
-			}); // her halükarda en son çalışır
-		console.log("Merhaba");
+		makeAsyncCall();
+		makeHttpCall();
 	}, []);
 
+	const makeAsyncCall = async () => {
+		// myAsyncFunction()
+		// .then(response => {
+		// 	console.log("İşlem başarılı cevap:", response);
+		// }) // async işlem başarılı ise (resolve edildi ise)
+		// .catch(error => {
+		// 	console.log("İşlem başarısız cevap:", error);
+		// }) // async işlem başarısız ise (reject edildi ise)
+		// .finally(() => {
+		// 	console.log("İşlem bitti");
+		// }); // her halükarda en son çalışır
+
+		//es2017 async-await
+		try {
+			let response = await myAsyncFunction();
+			console.log(response);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const makeHttpCall = async () => {
+		// fetch("https://dummyjson.com/products")
+		// 	.then(response => response.json())
+		// 	.then(json => console.log(json))
+		// 	.catch(err => console.log(err));
+		// zincirleme promise
+
+		let response = await fetch("https://dummyjson.com/products");
+		let json = await response.json();
+		console.log(json);
+	};
 	return (
 		<div>
 			<button className="btn btn-primary">Homepage</button>
