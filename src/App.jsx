8ff2bd55f2c7b347a2dec357885ce18f0948efb3
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 10:23:00 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 import About from "./pages/About/About";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Homepage />}></Route>
-				<Route path="/about" element={<About />}></Route>
-				<Route path="*" element={<p>Not Found</p>}></Route>
-			</Routes>
-		</BrowserRouter>
+		<>
+			<Navbar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Homepage />}></Route>
+					<Route path="/about" element={<About />}></Route>
+					<Route path="*" element={<p>Not Found</p>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 export default App;
