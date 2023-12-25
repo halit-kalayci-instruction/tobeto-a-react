66970450e016ab296d5247662593f3c98d2273dd
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 10:35:57 +0300

    then-catch-finally

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 
 export default function Homepage() {
+	const myAsyncFunction = () => {
+		return new Promise((resolve, reject) => {
+			reject("HTTP VERİSİ");
+		});
+	};
+
+	useEffect(() => {
+		myAsyncFunction()
+			.then(response => {
+				console.log("İşlem başarılı cevap:", response);
+			}) // async işlem başarılı ise (resolve edildi ise)
+			.catch(error => {
+				console.log("İşlem başarısız cevap:", error);
+			}) // async işlem başarısız ise (reject edildi ise)
+			.finally(() => {
+				console.log("İşlem bitti");
+			}); // her halükarda en son çalışır
+		console.log("Merhaba");
+	}, []);
+
 	return (
 		<div>
 			<button className="btn btn-primary">Homepage</button>
