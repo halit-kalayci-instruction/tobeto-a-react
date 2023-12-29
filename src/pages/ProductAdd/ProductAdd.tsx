bc44565e26842143b1a0eaacebc722a9eeca95de
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-29 09:48:34 +0300

    formik

--- src/pages/ProductAdd/ProductAdd.tsx
+++ src/pages/ProductAdd/ProductAdd.tsx
@@ -1,35 +1,50 @@
+import {Formik, Form, Field} from "formik";
 import React from "react";
 
 type Props = {};
 
 const ProductAdd = (props: Props) => {
+	const initialValues = {
+		title: "",
+		description: "",
+		price: 0,
+		stock: 0,
+	};
+
 	return (
 		<div className="container mt-5">
-			<form>
-				<div className="mb-3">
-					<label className="form-label">Ürün Adı</label>
-					<input type="text" className="form-control" />
-				</div>
+			<Formik
+				initialValues={initialValues}
+				onSubmit={values => {
+					console.log(values);
+				}}
+			>
+				<Form>
+					<div className="mb-3">
+						<label className="form-label">Ürün Adı</label>
+						<Field name="title" type="text" className="form-control" />
+					</div>
 
-				<div className="mb-3">
-					<label className="form-label">Ürün Açıklaması</label>
-					<input type="text" className="form-control" />
-				</div>
+					<div className="mb-3">
+						<label className="form-label">Ürün Açıklaması</label>
+						<Field name="description" type="text" className="form-control" />
+					</div>
 
-				<div className="mb-3">
-					<label className="form-label">Ürün Fiyatı</label>
-					<input type="text" className="form-control" />
-				</div>
+					<div className="mb-3">
+						<label className="form-label">Ürün Fiyatı</label>
+						<Field name="price" type="number" className="form-control" />
+					</div>
 
-				<div className="mb-3">
-					<label className="form-label">Ürün Stok</label>
-					<input type="text" className="form-control" />
-				</div>
+					<div className="mb-3">
+						<label className="form-label">Ürün Stok</label>
+						<Field name="stock" type="number" className="form-control" />
+					</div>
 
-				<button type="submit" className="btn btn-primary">
-					Kaydet
-				</button>
-			</form>
+					<button type="submit" className="btn btn-primary">
+						Kaydet
+					</button>
+				</Form>
+			</Formik>
 		</div>
 	);
 };
