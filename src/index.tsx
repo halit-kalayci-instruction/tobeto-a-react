a7b972ce7f5fd659d21e750e8eec615a09383f1e
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-08 10:27:47 +0300

    create Store

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {configureStore} from "./store/configureStore";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
 );
+
+const globalStore = configureStore();
+
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={globalStore}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
